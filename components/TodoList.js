905cd9997581a37ceb4e6aadf5338399fb3effd8
Emiliano Acerbi <eacerbip@gmail.com>
2022-04-01 14:44:18 -0300

    first commit

---
+++ components/TodoList.js
@@ -0,0 +1,42 @@
+import { Icon, Stack, Text } from '@chakra-ui/react'
+
+import { AiFillCheckSquare, AiOutlineBorder } from "react-icons/ai";
+
+
+const TodoList = ({todoList, toggleComplete}) => {
+
+  const todo = todoList.map(todo => {
+    return (
+      <Stack 
+        // textDecoration={todo.complete ? 'line-through' : null }
+        direction='row'
+        alignItems='center'
+        justifyContent='space-between'
+        key={todo.id}
+        cursor='pointer'
+        onClick={() => toggleComplete(todo.id)}
+        _hover={{bg: '#13133979'}}
+        paddingX={2.5}
+        paddingY={1.5}
+        transition='.1s background ease-in-out'
+        width='100%'
+      >
+        <Text
+          
+        >
+          - {todo.task}
+        </Text>
+
+        {todo.complete ? <Icon w={7} h={7} as={AiFillCheckSquare}/> : <Icon w={7} h={7} as={AiOutlineBorder} /> }
+      </Stack>
+    )
+  })
+
+  return (
+    <Stack spacing={3} direction='column' alignItems='flex-start' justifyContent='center'>
+      {todo}
+    </Stack>
+  ) 
+}
+
+export default TodoList
---
+++ styles/theme.js
@@ -0,0 +1,21 @@
+import { extendTheme } from '@chakra-ui/react'
+
+// Version 1: Using objects
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        minWidth: '100vh',
+        bg: '#f1f4fd',
+        color: 'white',
+      },
+    },
+  },
+})
+
+export default theme
+
